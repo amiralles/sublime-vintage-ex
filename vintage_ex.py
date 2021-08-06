ee172f6e0422d4eb0e08acd57b109e54f36e52d7
Ale Miralles <amiralles.net@gmail.com>
2021-08-06 12:10:41 -0300

    feat: jump to last edit back/forth
    This command allow us to jump to the previous cursor position the
    same way vim's builtin 'double back ticks' command does.

--- vintage_ex.py
+++ vintage_ex.py
@@ -110,3 +110,31 @@ class VintageExRunCommand(sublime_plugin.WindowCommand):
 
     def invalid_command(self, cmd):
         sublime.status_message("ERR - UNKNOWN COMMAND - " + cmd)
+
+
+# Jumps back and forth between the previous and next cursor positions,
+# similarly to vim's bultin backtick-backtick command.
+class BackticksJumpCommand(sublime_plugin.WindowCommand):
+    BWD=-1
+    FWD=1
+    next_jump_direction=BWD
+
+    def run(self):
+        if self.next_jump_direction == self.FWD:
+            self.run_command_on_active_view("jump_forward")
+        else:
+            self.run_command_on_active_view("jump_back")
+        self.toggle_next_jump_direction()
+
+    def toggle_next_jump_direction(self):
+        if self.next_jump_direction == self.FWD:
+            self.next_jump_direction = self.BWD
+        else:
+            self.next_jump_direction = self.FWD
+
+    def run_command_on_active_view(self, cmd):
+        view = self.window.active_view()
+        if view:
+            view.run_command(cmd)
+        else:
+            print("There is no active view.")
