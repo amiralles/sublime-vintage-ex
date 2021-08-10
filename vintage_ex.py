bd50a5ab62d441daa26d56b6b98dd5ea41f060b7
Ale Miralles <amiralles.net@gmail.com>
2021-08-10 12:24:40 -0300

    merge MRU into vintage_ex

--- vintage_ex.py
+++ vintage_ex.py
@@ -1,5 +1,17 @@
-import sublime
-import sublime_plugin
+import sublime, sublime_plugin
+import os
+from pathlib import Path
+
+# This file combines *vintage_ex*, *mru*, and *favorites* plugins.
+
+# TODO:
+# * Extract methods into "support_files" class and reuse them for MRU
+#   and Favorites (Tons of overlap there.)
+#
+# * Add setting for:
+#     - [MRU] Exclude extensions
+#     - [MRU] Set Max number of entries
+#     - [MRU] Make MRU behaves user wide (same as vim-mru)
 
 class VintageExPromptCommand(sublime_plugin.WindowCommand):
     def run(self):
@@ -15,6 +27,7 @@ class VintageExPromptCommand(sublime_plugin.WindowCommand):
 
 class VintageExRunCommand(sublime_plugin.WindowCommand):
     def run(self, cmd):
+        [cmd, *args] = cmd.split('/')
         {
             ":only": lambda: self.close_other_tabs(),
             ":w":    lambda: self.save(),
@@ -27,7 +40,36 @@ class VintageExRunCommand(sublime_plugin.WindowCommand):
             ":sp":   lambda: self.split(),
             ":vsp":  lambda: self.vertical_split(),
             ":e":    lambda: self.edit(),
+            # ":%s":   lambda: self.find_and_replace(args),
+            ":mru":  lambda: self.mru(),
+            ":clear_mru":  lambda: self.clear_mru(),
         }.get(cmd,   lambda: self.invalid_command(cmd))()
+
+    def mru(self):
+         self.window.run_command("mru")
+
+    def clear_mru(self):
+         self.window.run_command("mru_clear")
+
+    # TODO: Find a way to set "replace_with" text.
+    #       (It seems that there is no easy way to do this)
+    # def find_and_replace(self, args):
+    #     # TODO: Parse and apply options.
+    #     [find, replace, *opts] = args
+    #     self.window.run_command(
+    #         "show_panel", {
+    #             "panel": "replace",
+    #             "reverse": False,
+    #             "regex": True,
+    #             "case_sensitive": True,
+    #             "whole_word": False ,
+    #             "in_selection": False,
+    #             "wrap": True,
+    #             "highlight_matches": True
+    #         }
+    #     )
+
+    #     self.window.run_command('insert', {'characters': find})
 
     def reset_split(self):
         view = self.window.active_view()
@@ -111,7 +153,6 @@ class VintageExRunCommand(sublime_plugin.WindowCommand):
     def invalid_command(self, cmd):
         sublime.status_message("ERR - UNKNOWN COMMAND - " + cmd)
 
-
 # Jumps back and forth between the previous and next cursor positions,
 # similarly to vim's bultin backtick-backtick command.
 class BackticksJumpCommand(sublime_plugin.WindowCommand):
@@ -138,3 +179,105 @@ class BackticksJumpCommand(sublime_plugin.WindowCommand):
             view.run_command(cmd)
         else:
             print("There is no active view.")
+
+# This setting affects the number of entries we *show* on the quick panel. It
+# has no effect on the number of entries we store.
+MRU_QUICK_PANEL_MAX_ENTRIES = 30
+MRU_QUICK_PANEL_MAX_PATH_DISPLAY_LEN = 120
+MRU_FILE_NAME = ".sublime-mru"
+MRU_NO_FILES_MSG = "There are no recently used files."
+
+class Mru():
+    # Returns a list of key value pairs for each entry in the MRU file
+    # where the *key* is the caption we use for the quick_panel and *value*
+    # is the absolute file path that allows us to open the selected item.
+    def describe_mru_files(self):
+        files_descriptions = []
+        if self.mru_file_exists():
+            with open(self.mru_file_fullpath(), "r") as f:
+                for line in f:
+                    file_path = line.strip()
+                    if file_path:
+                        files_descriptions.append({
+                            "file_description": self.describe_file(file_path),
+                            "full_path": file_path
+                        }
+                    )
+        return files_descriptions
+
+    # Returns a list of MRU files paths.
+    def read_mru_file(self):
+        mru_files_paths = []
+        if self.mru_file_exists():
+            with open(self.mru_file_fullpath(), "r") as f:
+                for line in f:
+                    file_path = line.strip()
+                    if file_path:
+                        mru_files_paths.append(file_path)
+
+        return mru_files_paths
+
+    def update_mru_file(self, file_path):
+        mru_files_paths = self.read_mru_file()
+
+        if file_path in mru_files_paths:
+            mru_files_paths.remove(file_path)
+
+        mru_files_paths.insert(0, file_path)
+        with open(self.mru_file_fullpath(), "w") as f:
+            for file_path in mru_files_paths:
+                if file_path:
+                    f.write(file_path + os.linesep)
+
+    # Returns a string in the form of:
+    # foo.txt (/Users/jane/tmp/foo.txt)
+    def describe_file(self, file_path):
+        return os.path.basename(file_path) + " (" + self.project_relative_path(file_path) + ")"
+
+    def mru_file_exists(self):
+        full_path = self.mru_file_fullpath()
+        if Path(full_path).is_file():
+            return True
+
+    def project_relative_path(self, file_path):
+        return file_path.replace(self.current_project_path() + "/", "")[:MRU_QUICK_PANEL_MAX_PATH_DISPLAY_LEN]
+
+    def mru_file_fullpath(self):
+        home = os.path.expanduser("~")
+        return os.path.join(home, MRU_FILE_NAME)
+
+
+class MruOnSave(sublime_plugin.EventListener, Mru):
+    def on_activated_async(self, view):
+        self.update_mru_file(view.file_name())
+
+
+class MruClearCommand(sublime_plugin.WindowCommand, Mru):
+    def run(self):
+        os.remove(self.mru_file_fullpath())
+
+# Shows most recently used files in the current project.
+class MruCommand(sublime_plugin.WindowCommand, Mru):
+    def run(self):
+        files_descriptions = self.describe_current_project_mru_files()
+        if len(files_descriptions) > 0:
+            items = [x["file_description"] for x in files_descriptions]
+            self.window.show_quick_panel(
+                items=items[:MRU_QUICK_PANEL_MAX_ENTRIES],
+                on_select=lambda idx: self.open_file(files_descriptions, idx))
+        else:
+            print(MRU_NO_FILES_MSG)
+
+    def describe_current_project_mru_files(self):
+        return [d for d in self.describe_mru_files() if self.is_current_project_file(d["full_path"])]
+
+    def is_current_project_file(self, file_path):
+        return self.current_project_path() in file_path
+
+    def current_project_path(self):
+        folders = self.window.folders()
+        return folders[0] if len(folders) > 0 else ""
+
+    def open_file(self, mru_files, idx):
+        if idx >= 0:
+            self.window.open_file(mru_files[idx]['full_path'])
